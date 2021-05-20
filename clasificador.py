49f8766ccebb606a1ee28d23a46326f69018e14f
carsanvel <carvsk@gmail.com>
2021-05-20 11:13:56 +0100

    Modelo básico

---
+++ clasificador.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 18:25:01 2021
+
+@author: carvsk
+"""
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from keras.callbacks import EarlyStopping
+import keras
+from time import time
+from matplotlib import pyplot as plt 
+
+
+# DATA SOURCE --------------------------------------------------
+
+batch_size = 5
+print("hola")
+
+train_data_dir = './Datos_FSI/training'
+validation_data_dir = './Datos_FSI/validation'
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255
+#        rotation_range=15,
+#        zoom_range=0.1
+)
+
+validation_datagen = ImageDataGenerator(
+        rescale=1./255
+)
+
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+
+# MODEL --------------------------------------------------
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(150, 150, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+# TRAINING --------------------------------------------------
+
+epochs = 20
+
+es = EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=3, restore_best_weights=True)
+
+history = model.fit_generator(
+        train_generator,
+        epochs=epochs,
+        validation_data = validation_generator,
+        callbacks = [es]
+)   
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='validation accuracy')
+
+plt.title('Entrenamiento futbol')
+plt.xlabel('Épocas')
+plt.legend(loc="lower right")
+
+plt.show()
